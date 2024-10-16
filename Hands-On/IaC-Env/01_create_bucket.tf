5db31d0a3902f738654f6e7e61c22a4ca69d42c1
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-10-16 12:23:54 +0900

    Update 01_create_bucket.tf

--- Hands-On/IaC-Env/01_create_bucket.tf
+++ Hands-On/IaC-Env/01_create_bucket.tf
@@ -23,25 +23,3 @@ resource "aws_s3_bucket_website_configuration" "xweb_bucket_website" {
     suffix = "index.html"  # 기본 문서 설정
   }
 }
-
-# S3 버킷의 public read 정책 설정
-resource "aws_s3_bucket_policy" "public_read_access" {
-  bucket = aws_s3_bucket.bucket1.id  # 생성된 S3 버킷 이름 사용
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": [ "s3:GetObject" ],
-      "Resource": [
-        "arn:aws:s3:::ce28-bucket1",
-        "arn:aws:s3:::ce28-bucket1/*"
-      ]
-    }
-  ]
-}
-EOF
-}
